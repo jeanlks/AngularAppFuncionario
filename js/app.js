e2c132bbf9c1d46bf67fd8cdf075702c0eae00f9
Monte Carvalho <jlmonte@indra.es>
2016-10-13 07:59:02 -0300

    Corrigindo update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 var app = angular.module("App", ["firebase"]);
 
 
-app.controller("Ctrl", function ($scope, $firebaseArray) {
-  
+    app.controller("Ctrl", function ($scope,$window, $firebaseArray) {
+  var ref = new Firebase("https://crud-9011c.firebaseio.com/Funcionarios");
+    $scope.funcionarios = $firebaseArray(ref);
+  var ref = new Firebase("https://crud-9011c.firebaseio.com/Departamentos");
+     $scope.departamentos = $firebaseArray(ref);
+  var ref = new Firebase("https://crud-9011c.firebaseio.com/Cargos");
+     $scope.cargos = $firebaseArray(ref);
+    
   $scope.addFuncionario = function() {
-    var ref = new Firebase("https://crud-9011c.firebaseio.com/Funcionario");
-    $scope.funcionario = $firebaseArray(ref);
-    $scope.funcionario.$add({
+    $scope.funcionarios.$add({
       FUNC_CODE: $scope.newCodeFunc,
       FUNC_NOME: $scope.newNome,  
       FUNC_CPF: $scope.newCPF,
@@ -16,27 +20,53 @@ app.controller("Ctrl", function ($scope, $firebaseArray) {
       FUNC_SALARIO: $scope.newSalario,
       FUNC_CARGO: $scope.newCargo
     });
+        $window.alert("Funcionário Salvo: "+$scope.newNome); 
   };
-    
+  
+ $scope.editFuncionario = function(funcionario) {
+    $scope.funcionarios.$edit({
+      FUNC_CODE: $scope.newCodeFunc,
+      FUNC_NOME: $scope.newNome,  
+      FUNC_CPF: $scope.newCPF,
+      FUNC_DT_NASCIMENTO: $scope.newDTNascimento,
+      FUNC_DEPTO: $scope.newFuncDepto,
+      FUNC_DT_CONTRATACAO: $scope.newDTContratacao,
+      FUNC_SALARIO: $scope.newSalario,
+      FUNC_CARGO: $scope.newCargo
+    });
+        $window.alert("Funcionário Salvo: "+$scope.newNome); 
+  };
+        
+$scope.deleteFuncionario = function(funcionario) {
+    $window.alert("Funcionario Deletado"+funcionario.FUNC_NOME); 
+    $scope.funcionarios.$remove(funcionario);
+  };
+        
     $scope.addDepartamento = function(){
-     var ref = new Firebase("https://crud-9011c.firebaseio.com/Departamento");
-     $scope.departamento = $firebaseArray(ref);
-       $scope.departamento.$add({
+       $scope.departamentos.$add({
        DEPTO_CODE: $scope.newDeptoCode,
        DEPTO_DESCRICAO: $scope.newDescricao,
        DEPTO_RAMAL: $scope.newDeptoRamal
        });
+        $window.alert("Departamento Salvo: "+$scope.newDeptoCode); 
     };
     
+        $scope.deleteDepartamento = function(departamento) {
+        $scope.departamentos.$remove(departamento);
+        $window.alert("Departamento Deletado"+departamento.DEPTO_DESCRICAO); 
+    };
+   
     $scope.addCargo = function(){
-     var ref = new Firebase("https://crud-9011c.firebaseio.com/Cargo");
-     $scope.cargo = $firebaseArray(ref);
-      $scope.cargo.$add({
+      $scope.cargos.$add({
       CARGO_COD: $scope.newCargoCod,
       CARGO_DESCRICAO: $scope.newCargoDescricao,
       CARGO_NIVEL: $scope.newCargoNivel
       });
+      $window.alert("Cargo Salvo: "+$scope.newCargoCod); 
     };
-
+        $scope.deleteCargo = function(cargo) {
+        $scope.cargos.$remove(cargo);
+        $window.alert("Cargo Deletado"+cargo.CARGO_DESCRICAO); 
+  };
   // click on `index.html` above to see $remove() and $save() in action
 });
