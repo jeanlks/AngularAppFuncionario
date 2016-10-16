451107de2611523c16108e73cf318af1f1fcf69d
Jean Lucas Monte Carvalho <jean.rv@hotmail.com>
2016-10-16 14:23:04 -0200

    Refatorando

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var app = angular.module("App", ["firebase"]);
  
 
 app.controller("CtrlFuncionario", function ($scope,$window, $firebaseArray) {
- 
   $scope.btnEdit = "Editar";
   $scope.secondClick = false;
     
@@ -13,90 +12,80 @@ app.controller("CtrlFuncionario", function ($scope,$window, $firebaseArray) {
   $scope.funcionarios = $firebaseArray(ref);
   $scope.departamentos = $firebaseArray(refDepartamentos);
   $scope.cargos = $firebaseArray(refCargos);
-    
-   $scope.clearPage = function(){
+
+  
+  $scope.clearPage = function(){
          $scope.funcionarioForm =null;
-    }
-   
+  }
+  
   $scope.addFuncionario = function(funcionarioForm) {
     $scope.funcionarios.$add(funcionarioForm);
         $window.alert("Funcionário Salvo: "+funcionarioForm.FUNC_NOME); 
         $scope.clearPage();
   };
-    
-
-        
-$scope.deleteFuncionario = function(funcionario) {
-    $window.alert("Funcionario Deletado"+funcionario.FUNC_NOME); 
+             
+  $scope.deleteFuncionario = function(funcionario) {
     $scope.funcionarios.$remove(funcionario);
+    $window.alert("Funcionario Deletado"+funcionario.FUNC_NOME); 
+    
   };
         
- $scope.editFuncionario = function(funcionario,funcionarioForm){
-        
-        if(!$scope.secondClick){
-            $scope.funcionarioForm = funcionario;
-            $scope.secondClick = true;
-            $scope.btnEdit = "Salvar";
-           $window.alert("Edite os campos acima");
+  $scope.editFuncionario = function(funcionario,funcionarioForm){
+     if(!$scope.secondClick){
+        $scope.funcionarioForm = funcionario;
+        $scope.secondClick = true;
+        $scope.btnEdit = "Salvar";
+        $window.alert("Edite os campos acima");
        
         }else{
-      $scope.funcionarios.$remove(funcionario);
-     $scope.funcionarios.$add($scope.funcionarioForm);
-      
-     $window.alert("Funcionario Atualizado: "+$scope.funcionarioForm.FUNC_NOME);
-    
-       $scope.secondClick = false;
-       $scope.btnEdit = "Editar";
-         $scope.clearPage();
-        }
-
-    };
+             $scope.funcionarios.$remove(funcionario);
+             $scope.funcionarios.$add($scope.funcionarioForm);
+             $window.alert("Funcionario Atualizado: "+$scope.funcionarioForm.FUNC_NOME);
+             $scope.secondClick = false;
+             $scope.btnEdit = "Editar";
+             $scope.clearPage();
+        }};
     
  
 });
 
 app.controller("CtrlDepto", function ($scope,$window, $firebaseArray) {
-$scope.secondClick = false;
-$scope.btnEdit = "Editar";
-
+    $scope.secondClick = false;
+    $scope.btnEdit = "Editar";
     
- var ref = new Firebase("https://crud-9011c.firebaseio.com/Departamentos");
-     $scope.departamentos = $firebaseArray(ref);
-     $scope.clearPage = function(){
+//Conexão com banco
+var ref = new Firebase("https://crud-9011c.firebaseio.com/Departamentos");
+    $scope.departamentos = $firebaseArray(ref);
+    $scope.clearPage = function(){
          $scope.departamentoForm = null;   
     };
+    
    //adiciona departamento no array do firebase 
-   $scope.addDepartamento = function(departamentoForm){
-       $scope.departamentos.$add(departamentoForm);
-      
-        $window.alert("Departamento Salvo: "+$scope.departamentoForm.DEPTO_DESCRICAO);
-        $scope.clearPage();
+$scope.addDepartamento = function(departamentoForm){
+    $scope.departamentos.$add(departamentoForm);
+    $window.alert("Departamento Salvo: "+$scope.departamentoForm.DEPTO_DESCRICAO);
+    $scope.clearPage();
     };
     
-        $scope.deleteDepartamento = function(departamentoForm) {
-        $scope.departamentos.$remove(departamentoForm);
-        $window.alert("Departamento Deletado: "+departamentoForm.DEPTO_DESCRICAO); 
+$scope.deleteDepartamento = function(departamentoForm) {
+    $scope.departamentos.$remove(departamentoForm);
+    $window.alert("Departamento Deletado: "+departamentoForm.DEPTO_DESCRICAO); 
     };
   
 
-    $scope.editDepartamento= function(departamento,departamentoForm){
-        
-        if(!$scope.secondClick){
-            $scope.departamentoForm = departamento;
-            $scope.secondClick = true;
-            $scope.btnEdit = "Salvar";
-            
-           $window.alert("Edite o campo acima");
-       
-        }else{
-        $scope.departamentos.$remove(departamento);
-        $scope.departamentos.$add($scope.departamentoForm);
-        
-       $window.alert("Departamento Atualizado: "+departamentoForm.DEPTO_DESCRICAO);
-       
-       $scope.secondClick = false;
-       $scope.btnEdit = "Editar";
-         $scope.clearPage();
+$scope.editDepartamento= function(departamento,departamentoForm){
+    if(!$scope.secondClick){
+        $scope.departamentoForm = departamento;
+        $scope.secondClick = true;
+        $scope.btnEdit = "Salvar";
+        $window.alert("Edite o campo acima");
+       }else{
+            $scope.departamentos.$remove(departamento);
+            $scope.departamentos.$add($scope.departamentoForm);
+            $window.alert("Departamento Atualizado: "+departamentoForm.DEPTO_DESCRICAO);
+            $scope.secondClick = false;
+            $scope.btnEdit = "Editar";
+            $scope.clearPage();
         }
 
     };
@@ -110,46 +99,38 @@ app.controller("CtrlCargo", function ($scope,$window, $firebaseArray) {
     $scope.btnEdit = "Editar";
     
     var ref = new Firebase("https://crud-9011c.firebaseio.com/Cargos");
-     $scope.cargos = $firebaseArray(ref);
+    $scope.cargos = $firebaseArray(ref);
     
-     $scope.clearPage = function(){
-         $scope.cargoForm= null;   
+ $scope.clearPage = function(){
+      $scope.cargoForm= null;   
      };
     
-    $scope.addCargo = function(cargoForm){
+ $scope.addCargo = function(cargoForm){
       $scope.cargos.$add(cargoForm);
       $window.alert("Cargo Salvo: "+cargoForm.CARGO_DESCRICAO); 
       $scope.clearPage();
     };
-        $scope.deleteCargo = function(cargo) {
-        $scope.cargos.$remove(cargo);
-        $window.alert("Cargo Deletado: "+cargo.CARGO_DESCRICAO); 
-        
-  };
+ $scope.deleteCargo = function(cargo) {
+      $scope.cargos.$remove(cargo);
+      $window.alert("Cargo Deletado: "+cargo.CARGO_DESCRICAO); 
+    };
     
-    $scope.editCargo= function(cargo,cargoForm){
-        
-        if(!$scope.secondClick){
+ $scope.editCargo= function(cargo,cargoForm){
+    if(!$scope.secondClick){
             $scope.cargoForm = cargo;
             $scope.secondClick = true;
             $scope.btnEdit = "Salvar";
-            
-           $window.alert("Edite os campos acima");
-       
-        }else{
-       $scope.cargos.$remove(cargo);
-        $scope.cargos.$add($scope.cargoForm);
-        
-       $window.alert("Cargo Atualizado: "+cargoForm.CARGO_DESCRICAO);
-       
-       $scope.secondClick = false;
-       $scope.btnEdit = "Editar";
-         $scope.clearPage();
+            $window.alert("Edite os campos acima");
+       }else{
+            $scope.cargos.$remove(cargo);
+            $scope.cargos.$add($scope.cargoForm);
+            $window.alert("Cargo Atualizado: "+cargoForm.CARGO_DESCRICAO);
+            $scope.secondClick = false;
+            $scope.btnEdit = "Editar";
+            $scope.clearPage();
         }
 
     };
-    
-
-        
+            
 });
 
